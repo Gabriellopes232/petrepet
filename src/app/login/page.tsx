b63e755e3039e09293cdd7c9cc29e5ab48eaac9a
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2024-11-25 21:10:20 -0300

    feat: finishing login screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import Image from 'next/image'
-import { Box, Button, Typography } from '@mui/material';
-import { LogoTextContainer, MainContainerStyled, TextContainer } from './styles';
+import React from 'react';
+import Image from 'next/image';
+import { Button, Box, Typography, Divider } from '@mui/material';
+import {
+  MainContainerStyled,
+  LogoContainer,
+  FindYourPetContainer,
+  FormContainer,
+  FormContent,
+} from './styles';
 import FindYourPet from '@/components/Login/FindYourPet';
 import EmailIcon from '@mui/icons-material/Email';
 import KeyIcon from '@mui/icons-material/Key';
@@ -11,97 +17,258 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 
 export default function Login() {
   const backgroundImage = "/backgroundLogin.png";
-  const logoPetRepet = "/logo.png"
+  const logoPetRepet = "/logo.png";
+
   return (
-    <MainContainerStyled aria-label='main container'>
+    <MainContainerStyled aria-label="main container">
       <Image
         src={backgroundImage}
         width={1500}
         height={1500}
         quality={100}
-        alt='Background dog'
+        alt="Background dog"
         style={{
           objectFit: 'cover',
           width: '100vw',
           height: '100vh',
-          position: "absolute",
+          position: 'absolute',
           zIndex: -1,
         }}
       />
-      <LogoTextContainer aria-label='logo top container'>
-        <Box>
-          <Image
-            src={logoPetRepet}
-            width={100}
-            height={100}
-            quality={100}
-            alt='Logo petrepet'
+
+      <LogoContainer>
+        <Image
+          src={logoPetRepet}
+          width={100}
+          height={100}
+          quality={100}
+          alt="Logo petrepet"
+        />
+      </LogoContainer>
+
+      <FindYourPetContainer>
+        <FindYourPet />
+      </FindYourPetContainer>
+
+      <FormContainer>
+        <FormContent>
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{
+              fontWeight: 'bold',
+              color: '#3E7CB1',
+            }}
+          >
+            Olá,{' '}
+            <br />
+            <Typography
+              variant="body1"
+              component="span"
+              sx={{
+                fontSize: 24,
+                fontWeight: 'regular',
+                color: '#3E7CB1',
+              }}
+            >
+              Coloque o login e a senha para continuar
+            </Typography>
+          </Typography>
+
+          <CustomTextField
+            type="email"
+            placeholder="Email"
+            icon={<EmailIcon sx={{
+              color: '#81A4CD'
+            }} />}
+            required
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: '20px',
+                height: '50px',
+                '& fieldset': {
+                  borderColor: '#81A4CD',
+                  borderWidth: '1px',
+                  background: 'none'
+                },
+                '&:hover fieldset': {
+                  borderColor: '#81A4CD',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#81A4CD',
+                  background: 'none'
+                },
+              },
+              '& .MuiInputBase-input': {
+                padding: '10px 15px',
+                fontSize: '1rem',
+                background: 'none',
+                color: '#3E7CB1',
+              },
+            }}
           />
-        </Box>
 
-        <TextContainer>
-          <FindYourPet />
-        </TextContainer>
-      </LogoTextContainer>
+          <CustomTextField
+            type="password"
+            placeholder="Senha"
+            icon={<KeyIcon sx={{
+              color: '#81A4CD'
+            }} />}
+            required
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: '20px',
+                height: '50px',
+                '& fieldset': {
+                  borderColor: '#81A4CD',
+                  borderWidth: '1px',
+                  background: 'none'
+                },
+                '&:hover fieldset': {
+                  borderColor: '#81A4CD',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#81A4CD',
+                },
+              },
+              '& .MuiInputBase-input': {
+                padding: '10px 15px',
+                fontSize: '1rem',
+                background: 'none',
+                color: '#3E7CB1',
+              },
+            }}
+          />
 
-      <Box>
-        <Typography
-          variant="h4"
-          color="initial"
-        >
-          Olá,
-        </Typography>
-        <Typography
-          color="initial"
-          variant="body1"
-          component="p"
-        >
-          Coloque o login e a senha para continuar
-        </Typography>
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            sx={{
+              textTransform: 'none',
+              fontSize: '1rem',
+              padding: '10px 20px',
+              borderRadius: '25px',
+              height: '70%',
+              backgroundColor: '#3E7CB1',
+              color: 'white',
+              fontWeight: 'bold',
+              '&:hover': {
+                backgroundColor: '#316C9B',
+              },
+            }}
+            fullWidth
+          >
+            Entrar
+          </Button>
 
-        <CustomTextField
-          type="email"
-          placeholder="Email"
-          icon={<EmailIcon />}
-          required
-        />
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Typography
+              variant="body1"
+              component="span"
+              sx={{
+                fontSize: 16,
+                fontWeight: 'regular',
+                color: '#3E7CB1',
+              }}
+            >ou</Typography>
+          </Box>
 
-        <CustomTextField
-          type="password"
-          placeholder="Senha"
-          icon={<KeyIcon />}
-          required
-        />
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 10
+          }}>
+            <Button
+              variant="outlined"
+              sx={{
+                textTransform: 'none',
+                fontSize: '1rem',
+                padding: '10px 20px',
+                borderRadius: '25px',
+                borderColor: '#3E7CB1',
+                color: '#3E7CB1',
+                width: '50%',
+                fontWeight: 'bold',
+                '&:hover': {
+                  borderColor: '#316C9B',
+                  backgroundColor: '#F0F7FF',
+                },
+              }}
+            >
+              <GoogleIcon sx={{ marginRight: '10px', color: '#3E7CB1' }} />
+              Google
+            </Button>
+            <Button
+              variant="outlined"
+              sx={{
+                textTransform: 'none',
+                fontSize: '1rem',
+                padding: '10px 20px',
+                borderRadius: '25px',
+                borderColor: '#3E7CB1',
+                color: '#3E7CB1',
+                fontWeight: 'bold',
+                width: '50%',
+                '&:hover': {
+                  borderColor: '#316C9B',
+                  backgroundColor: '#F0F7FF',
+                },
+              }}
+            >
+              <FacebookIcon sx={{ marginRight: '10px', color: '#3E7CB1' }} />
+              Facebook
+            </Button>
+          </Box>
 
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          size="large"
-          sx={{
-            textTransform: "none",
-            fontSize: "1rem",
-            padding: "10px",
-          }}
-          fullWidth
-        >
-          Entrar
-        </Button>
+          <Box sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            <a href="#" style={{
+              color: '#81A4CD'
+            }}>Esqueci a senha</a>
+          </Box>
 
-        <Button>
-          <GoogleIcon />
-        </Button>
-        <Button>
-          <FacebookIcon />
-        </Button>
+          <Divider
+            sx={{
+              color: '#81A4CD'
+            }}
+          ></Divider>
 
-        <a>Esqueci a senha</a>
-
-        <div>divider</div>
-
-        <p>Ao logar você concorda com os <a>Termos e Condições</a></p>
-        <p>Não tem uma conta ainda? <a href="">Criar conta</a></p>
-      </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <p style={{
+              color: '#9D9D9D'
+            }}>
+              Ao logar você concorda com os <a href="#" style={{
+                color: '#81A4CD'
+              }}>Termos e Condições</a>
+            </p>
+            <p style={{
+              color: '#9D9D9D'
+            }}>
+              Não tem uma conta ainda? <a href="#" style={{
+                color: '#81A4CD'
+              }}>Criar conta</a>
+            </p>
+          </Box>
+        </FormContent>
+      </FormContainer>
     </MainContainerStyled>
-  )
+  );
 }
--- src/app/login/styles.ts
+++ src/app/login/styles.ts
@@ -7,22 +7,41 @@ export const MainContainerStyled = styled(Box)(() => ({
   width: '100vw',
   height: '100vh',
   display: 'flex',
-  flexDirection: 'column',
-}))
+  flexDirection: 'row',
+}));
 
-export const LogoTextContainer = styled(Box)(() => ({
+export const LogoContainer = styled(Box)(() => ({
+  position: 'absolute',
+  top: '15px',
+  left: '15px',
+}));
+
+export const FindYourPetContainer = styled(Box)(() => ({
+  position: 'absolute',
+  bottom: '50px',
+  left: '50px',
+}));
+
+export const FormContainer = styled(Box)(() => ({
   display: 'flex',
   flexDirection: 'column',
+  justifyContent: 'center',
   alignItems: 'flex-start',
+  padding: '30px',
+  width: '50%',
   height: '100%',
-  padding: '15px',
-  position: 'relative',
-  flex: 1
-}))
+  marginLeft: 'auto',
+}));
 
-export const TextContainer = styled(Box)(() => ({
-  textAlign: 'left',
-  marginBottom: '15px',
-  marginLeft: '15px',
-  padding: '15px'
-}))
+export const FormContent = styled(Box)(() => ({
+  background: 'white',
+  borderRadius: '35px',
+  height: '80%',
+  width: '60%',
+  position: 'relative',
+  left: '150px',
+  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
+  border: 'none',
+  padding: '2rem',
+  display: 'grid'
+}));
